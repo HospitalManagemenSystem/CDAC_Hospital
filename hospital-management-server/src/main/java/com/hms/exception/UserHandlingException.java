3372f078634b6ee18aed8989c2fd37058995425c
Atharva1479 <atharvajamdar1810@gmail.com>
2025-02-04 23:29:21 +0530

    Added custom exception

--- hospital-management-server/src/main/java/com/hms/exception/UserHandlingException.java
+++ hospital-management-server/src/main/java/com/hms/exception/UserHandlingException.java
@@ -1,5 +1,10 @@
 package com.hms.exception;
 
-public class UserHandlingException {
-    
+public class UserHandlingException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UserHandlingException(String message) {
+        super(message);
+    }
 }
