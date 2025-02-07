daa5611b7aa1976f8d0f0a9d490d7e8a80b4fb93
dipakghule2106 <ghuledipak2106@gmail.com>
2025-02-08 00:02:56 +0530

    Added Admin Service Interface

---
+++ hospital-management-server/src/main/java/com/hms/service/AdminServiceIntf.java
@@ -0,0 +1,13 @@
+package com.hms.service;
+
+import java.util.List;
+
+import com.hms.entity.Admin;
+
+public interface AdminServiceIntf {
+
+	//get admin list
+	List<Admin> getListOfAdmin();
+	
+}
+
