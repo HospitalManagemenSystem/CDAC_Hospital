5445dc6355767f97fe45dbdef12e31a683b29055
Atharva1479 <atharvajamdar1810@gmail.com>
2025-02-08 13:20:03 +0530

    Modified Doctor Controller

--- hospital-management-server/src/main/java/com/hms/controller/DoctorController.java
+++ hospital-management-server/src/main/java/com/hms/controller/DoctorController.java
@@ -1,5 +1,6 @@
 package com.hms.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hms.dto.DoctorDTO;
 import com.hms.entity.Doctor;
+import com.hms.entity.DoctorTimeTable;
 import com.hms.service.DoctorService;
 
 @RestController
@@ -36,5 +43,21 @@ public class DoctorController {
 		
 		List<Doctor> doctors = doctorService.getDoctorsBySpecializationAndCity(specialization, city);
 		return new ResponseEntity<>(doctors, HttpStatus.OK);
+	}
+	
+	@PostMapping("/createAppointmentSlot/{doctorId}")
+	public List<LocalDateTime> createAppointmentSlots(@PathVariable Long doctorId,
+			@RequestBody DoctorTimeTable doctorTimeTable) {
+		return doctorService.createAvailableSlotsDetails(doctorId, doctorTimeTable);
+	}
+
+	@GetMapping("/getDoctorDetails/{doctorId}")
+	public ResponseEntity<?> getDoctorDetails(@PathVariable Long doctorId) {
+		return ResponseEntity.ok(doctorService.getDoctorDetails(doctorId));
+	}
+
+	@PutMapping("/updateDoctor/{doctorId}")
+	public ResponseEntity<?> updateDoctorDetails(@RequestBody DoctorDTO detachedDoctor, @PathVariable Long doctorId) {
+		return ResponseEntity.ok(doctorService.updateDoctorDetails(detachedDoctor, doctorId));
 	}
 }
